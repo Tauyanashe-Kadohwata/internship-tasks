d0dc9fcb6837ddda067e356ecb9dc825a567dd93
Tauyanashe Kadohwata <Tauyanashekadohwata@gmail.com>
2025-08-14 15:02:17 +0200

    Update 5th_query.sql

--- Task 3/5th_query.sql
+++ Task 3/5th_query.sql
@@ -1,22 +1,33 @@
+WITH ActorRanks AS (
+    SELECT
+        a.first_name,
+        a.last_name,
+        COUNT(f.film_id) AS film_count,
+        DENSE_RANK() OVER (ORDER BY COUNT(f.film_id) DESC) AS ranking
+    FROM
+        public.actor AS a
+    INNER JOIN
+        public.film_actor AS fa ON a.actor_id = fa.actor_id
+    INNER JOIN
+        public.film AS f ON fa.film_id = f.film_id
+    INNER JOIN
+        public.film_category AS fc ON f.film_id = fc.film_id
+    INNER JOIN
+        public.category AS c ON fc.category_id = c.category_id
+    WHERE
+        c.name = 'Children'
+    GROUP BY
+        a.actor_id, a.first_name, a.last_name
+)
 SELECT
-    a.first_name,
-    a.last_name,
-    COUNT(f.film_id) AS film_count
+    first_name,
+    last_name,
+    film_count
 FROM
-    public.actor AS a
-INNER JOIN
-    public.film_actor AS fa ON a.actor_id = fa.actor_id
-INNER JOIN
-    public.film AS f ON fa.film_id = f.film_id
-INNER JOIN
-    public.film_category AS fc ON f.film_id = fc.film_id
-INNER JOIN
-    public.category AS c ON fc.category_id = c.category_id
+    ActorRanks
 WHERE
-    c.name = 'Children'
-GROUP BY                
-    a.actor_id, a.first_name, a.last_name
+    ranking <= 3
 ORDER BY
-    film_count DESC
-LIMIT 3;
+    film_count DESC;
+
 
