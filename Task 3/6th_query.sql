5866d12de0b20e0006955c52bfbb996ee52ea46f
Tauyanashe Kadohwata <Tauyanashekadohwata@gmail.com>
2025-08-14 14:58:33 +0200

    Update 6th_query.sql

--- Task 3/6th_query.sql
+++ Task 3/6th_query.sql
@@ -1,1 +1,15 @@
+SELECT
+    c.city,
+    SUM(CASE WHEN cust.active = 1 THEN 1 ELSE 0 END) AS active_customer_count,
+    SUM(CASE WHEN cust.active = 0 THEN 1 ELSE 0 END) AS inactive_customer_count
+FROM
+    public.city AS c
+INNER JOIN
+    public.address AS a ON c.city_id = a.city_id
+INNER JOIN
+    public.customer AS cust ON a.address_id = cust.address_id
+GROUP BY
+    c.city_id, c.city
+ORDER BY
+    inactive_customer_count DESC;
 
